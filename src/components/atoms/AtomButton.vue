a19b69697533e46cc07a45604b046018486d56d2
Adrian <adrianmalara506@gmail.com>
2023-10-08 14:05:47 +0200

    Pinia & Atomic design

---
+++ src/components/atoms/AtomButton.vue
@@ -0,0 +1,13 @@
+<template>
+    <button :type="type">{{ value }}</button>
+</template> 
+
+
+<script>
+export default {
+    props:{
+        type:String,
+        value:String
+    },
+}
+</script>
